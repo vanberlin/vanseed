f2ec748fdca761958fedd3eb44e6a11f5a7efba8
patrick <patrick@patrickmenke.com>
2019-06-22 07:50:19 +0200

    load CF sample block

---
+++ inc/carbonfields/theme-options.php
@@ -0,0 +1,10 @@
+<?php
+
+//use Carbon_Fields\Container;
+//use Carbon_Fields\Field;
+//
+//Container::make( 'theme_options', __( 'Theme Options' ) )
+//    ->add_fields( array(
+//        Field::make( 'text', 'crb_facebook_url', __( 'Facebook URL' ) ),
+//        Field::make( 'textarea', 'crb_footer_text', __( 'Footer Text' ) )
+//    ) );
