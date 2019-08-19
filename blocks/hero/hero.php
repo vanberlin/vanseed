82693805c5f3f14a164ce5197ea6893c57c3b22d
patrick <bitbucket_patorus@patorus.com>
2019-08-19 05:32:06 +0200

    better hero updated reqs

--- blocks/hero/hero.php
+++ blocks/hero/hero.php
@@ -2,28 +2,42 @@
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
 
-Block::make( __( 'My Shiny Gutenberg Block' ) )
-    ->add_fields( array(
-        Field::make( 'text', 'heading', __( 'Block Heading' ) ),
-        Field::make( 'image', 'image', __( 'Block Image' ) ),
-        Field::make( 'rich_text', 'content', __( 'Block Content' ) ),
-    ) )
-    ->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
-        ?>
+Block::make(__('Hero'))
+	->add_fields(array(
+		Field::make('select', 'heading_align', __('Heading align'))
+			->add_options(array(
+				'text-center' => 'Text center',
+				'text-left' => 'Text left',
+			)),
+		Field::make('image', 'image', __('Background Image')),
+		Field::make('text', 'heading_first', __('Heading first Line')),
+		Field::make('text', 'heading_second', __('Heading second Line')),
+		Field::make('text', 'cta_text', __('CTA Text')),
+		Field::make('text', 'cta_link', __('CTA Link')),
+	))
+	->set_category('vanberlin', __('vanBerlin'), 'smiley')
+	->set_render_callback(function ($fields, $attributes, $inner_blocks) {
+		?>
+		<div class="hero row">
+			<?php if ($fields['image']) {
+				echo wp_get_attachment_image($fields['image'], 'full', false, array('class' => 'hero__image'));
+			} else {
+				echo '<div class="hero__no-image"></div>';
+			} ?>
 
-        <div class="block">
-            <div class="block__heading">
-                <h1><?php echo esc_html( $fields['heading'] ); ?></h1>
-            </div><!-- /.block__heading -->
+			<div class="hero__body">
+				<div class="container">
+					<h1 class="hero__heading <?php echo $fields['heading_align']; ?>">
+						<span class="hero__heading-first"><?= esc_html($fields['heading_first']); ?></span>
+						<span class="hero__heading-second"><?= esc_html($fields['heading_second']); ?></span>
+					</h1>
 
-            <div class="block__image">
-                <?php echo wp_get_attachment_image( $fields['image'], 'full' ); ?>
-            </div><!-- /.block__image -->
+					<?php if ($fields['cta_text']): ?>
+						<a class="hero__cta btn btn-lg btn-primary" href="<?php echo $fields['cta_link'] ?>"><?php echo $fields['cta_text']; ?></a>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
 
-            <div class="block__content">
-                <?php echo apply_filters( 'the_content', $fields['content'] ); ?>
-            </div><!-- /.block__content -->
-        </div><!-- /.block -->
-
-        <?php
-    } );
+		<?php
+	});
