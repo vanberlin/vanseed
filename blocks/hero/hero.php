99460f6eac95ee62de1a8870038d48e658b3aaf9
patrick <bitbucket_patorus@patorus.com>
2019-12-08 14:40:54 +0100

    new default files hero: image as background

--- blocks/hero/hero.php
+++ blocks/hero/hero.php
@@ -1,43 +1,40 @@
 <?php
+
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
 
 Block::make(__('Hero'))
 	->add_fields(array(
-		Field::make('select', 'heading_align', __('Heading align'))
-			->add_options(array(
-				'text-center' => 'Text center',
-				'text-left' => 'Text left',
-			)),
 		Field::make('image', 'image', __('Background Image')),
-		Field::make('text', 'heading_first', __('Heading first Line')),
-		Field::make('text', 'heading_second', __('Heading second Line')),
-		Field::make('text', 'cta_text', __('CTA Text')),
-		Field::make('text', 'cta_link', __('CTA Link')),
+		Field::make('image', 'image_mobile', __('Background Image Mobile')),
+		Field::make('textarea', 'headline', __('Headline')),
 	))
 	->set_category('vanberlin', __('vanBerlin'), 'smiley')
 	->set_render_callback(function ($fields, $attributes, $inner_blocks) {
 		?>
-		<div class="hero row">
+		<section class="hero">
 			<?php if ($fields['image']) {
-				echo wp_get_attachment_image($fields['image'], 'full', false, array('class' => 'hero__image'));
+				$heroimage = wp_get_attachment_image_src($fields['image'], 'full', false);
+				$heroimageMobile = wp_get_attachment_image_src($fields['image_mobile'], 'full', false);
 			} else {
-				echo '<div class="hero__no-image"></div>';
+				$heroimage[0] = "";
 			} ?>
+			<style>
+				.hero {
+					background-image: url('<?= $heroimageMobile[0]; ?>');
+				}
 
-			<div class="hero__body">
-				<div class="container">
-					<h1 class="hero__heading <?php echo $fields['heading_align']; ?>">
-						<span class="hero__heading-first"><?= esc_html($fields['heading_first']); ?></span>
-						<span class="hero__heading-second"><?= esc_html($fields['heading_second']); ?></span>
-					</h1>
+				@media (min-width: 576px) {
+					.hero {
+						background-image: url('<?= $heroimage[0]; ?>');
+					}
+				}
+			</style>
 
-					<?php if ($fields['cta_text']): ?>
-						<a class="hero__cta btn btn-lg btn-primary" href="<?php echo $fields['cta_link'] ?>"><?php echo $fields['cta_text']; ?></a>
-					<?php endif; ?>
-				</div>
+			<div class="headline">
+				<h1><?= nl2br($fields['headline']) ?></h1>
 			</div>
-		</div>
+		</section>
 
 		<?php
 	});
